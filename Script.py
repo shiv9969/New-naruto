96e0f2f89c435f2f10d5465099eb75132af586f5
shiv9969 <150778501+shiv9969@users.noreply.github.com>
2025-04-17 15:01:04 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -310,7 +310,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/SilentXBotz">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a https://t.me/SilentXBotz>[SilentXBotz]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/BoB_Files1">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a https://t.me/>[BoB Files]</a></BoB_Files1b>"""
 
     IMDB_TEMPLATE_TXT = """
 <b>🏷 Title</b>: <a href={url}>{title}</a>
